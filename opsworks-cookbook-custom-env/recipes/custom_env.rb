569a7e0ef26719fd8100d6ed48ddfd7ad5f7a05a
Lucas Ocon <lucas_masterclas@hotmail.com>
2019-06-12 23:30:10 +0200

    Add logs for env variables

--- opsworks-cookbook-custom-env/recipes/custom_env.rb
+++ opsworks-cookbook-custom-env/recipes/custom_env.rb
@@ -2,6 +2,14 @@
 Chef::Log.info("Create config/application.yml file...")
 
 node[:deploy].each do |application, deploy|
+  Chef::Log.info("Debugg env variables")
+  Chef::Log.info(">>>>>>> custom_env")
+  Chef::Log.info(deploy[:custom_env].to_h)
+  Chef::Log.info(">>>>>>> environment_variables")
+  Chef::Log.info(deploy[:environment_variables].to_h)
+  Chef::Log.info(">>>>>>> environment")
+  Chef::Log.info(deploy[:environment].to_h)
+
   environment_variables = deploy[:custom_env].to_h.merge(deploy[:environment_variables].to_h)
 
   custom_env_template do
